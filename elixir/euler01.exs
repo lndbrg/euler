ce69a9ed84cba444fe2e3f22e7c77fb709b81640
olle <geek@nerd.sh>
2013-12-14 23:58:47 +0100

    First euler in elixir.

---
+++ elixir/euler01.exs
@@ -0,0 +1,17 @@
+defmodule Euler.Problem1 do
+
+  def solver(n) when n <= 0 do 0 end
+
+  def solver(n) when ((rem(n, 3) == 0) or (rem(n, 5) == 0)) do
+    n + solver(n-1)
+  end
+
+  def solver(n) do
+     solver(n-1)
+  end
+
+  def solve(limit) do
+     IO.puts solver(limit-1)
+  end
+
+end
